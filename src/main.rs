40a580e3ecc76ace98fd7c2ef93e5a09ead511b2
suap-dev <suap.dev@gmail.com>
2023-10-11 01:56:15 +0200

    Circle is centered.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::all, clippy::nursery, clippy::pedantic)]
 
 use ggez::{
-    glam::vec2,
+    glam::{vec2, Vec2},
     graphics::{self, Color},
     Context, GameResult,
 };
@@ -11,26 +11,35 @@ fn main() {
         let context_builder = ggez::ContextBuilder::new("Thing", "suap");
         context_builder
             .build()
-            .expect("Failed to create ggex context.")
+            .expect("Failed to create ggez context.")
     };
 
-    let thing = Thing {
-        circle: graphics::Mesh::new_circle(
-            &context,
-            graphics::DrawMode::fill(),
-            vec2(0.0, 0.0),
-            50.0,
-            0.5,
-            Color::WHITE,
-        ).unwrap(),
-    };
+    let thing = Thing::new(&context);
 
     ggez::event::run(context, event_loop, thing);
 }
 
 struct Thing {
     circle: graphics::Mesh,
+    circle_pos: Vec2,
+}
+impl Thing {
+    fn new(context: &Context) -> Self {
+        let (width, height) = context.gfx.drawable_size();
 
+        Self {
+            circle: graphics::Mesh::new_circle(
+                context,
+                graphics::DrawMode::fill(),
+                vec2(0.0, 0.0),
+                50.0,
+                0.2,
+                Color::WHITE,
+            )
+            .unwrap(),
+            circle_pos: vec2(width / 2.0, height / 2.0),
+        }
+    }
 }
 impl ggez::event::EventHandler for Thing {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
@@ -40,7 +49,7 @@ impl ggez::event::EventHandler for Thing {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
 
-        canvas.draw(&self.circle, vec2(100.0, 100.0));
+        canvas.draw(&self.circle, self.circle_pos);
 
         canvas.finish(ctx)
     }
