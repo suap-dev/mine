9f3b65b508c88525acfc5d0e4029f9228da194e5
suap-dev <suap.dev@gmail.com>
2023-10-11 02:15:43 +0200

    Error propagation.

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,41 @@ use ggez::{
     Context, GameResult,
 };
 
-fn main() {
+fn main() -> GameResult {
     let (context, event_loop) = {
         let context_builder = ggez::ContextBuilder::new("Thing", "suap");
-        context_builder
-            .build()
-            .expect("Failed to create ggez context.")
+        context_builder.build()?
     };
 
-    let thing = Thing::new(&context);
+    let thing = Thing::new(&context)?;
 
     ggez::event::run(context, event_loop, thing);
 }
 
 struct Thing {
     circle: graphics::Mesh,
-    circle_pos: Vec2,
+    circle_position: Vec2,
 }
 impl Thing {
-    fn new(context: &Context) -> Self {
-        let (width, height) = context.gfx.drawable_size();
+    fn new(context: &Context) -> GameResult<Self> {
+        let circle = graphics::Mesh::new_circle(
+            context,
+            graphics::DrawMode::fill(),
+            vec2(0.0, 0.0),
+            50.0,
+            0.2,
+            Color::WHITE,
+        )?;
 
-        Self {
-            circle: graphics::Mesh::new_circle(
-                context,
-                graphics::DrawMode::fill(),
-                vec2(0.0, 0.0),
-                50.0,
-                0.2,
-                Color::WHITE,
-            )
-            .unwrap(),
-            circle_pos: vec2(width / 2.0, height / 2.0),
-        }
+        let circle_position = {
+            let (width, height) = context.gfx.drawable_size();
+            vec2(width / 2.0, height / 2.0)
+        };
+
+        Ok(Self {
+            circle,
+            circle_position,
+        })
     }
 }
 impl ggez::event::EventHandler for Thing {
@@ -49,7 +51,7 @@ impl ggez::event::EventHandler for Thing {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
 
-        canvas.draw(&self.circle, self.circle_pos);
+        canvas.draw(&self.circle, self.circle_position);
 
         canvas.finish(ctx)
     }
